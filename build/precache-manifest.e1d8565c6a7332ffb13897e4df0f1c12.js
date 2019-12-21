ea11d7715900b2bab4ec7e076c657bc1b14b5c66
Allan Wilson <allan@allanwilson.net>
2019-12-22 00:00:24 +0200

    .

---
+++ build/precache-manifest.e1d8565c6a7332ffb13897e4df0f1c12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9df1d68f94aab4444ab6",
+    "url": "/static/js/main.9df1d68f.chunk.js"
+  },
+  {
+    "revision": "66a8571e0e774556d6c2",
+    "url": "/static/js/1.66a8571e.chunk.js"
+  },
+  {
+    "revision": "9df1d68f94aab4444ab6",
+    "url": "/static/css/main.94d090aa.chunk.css"
+  },
+  {
+    "revision": "25c9d86ede22bf6ca5085922ee637c1a",
+    "url": "/index.html"
+  }
+];
